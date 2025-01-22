958d427345fd735f45be2b5f1b4ae2889cab1f4b
LuCorGon <cortegini@gmail.com>
2025-01-22 20:11:18 +0100

    control de errores al subir el video

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from communicator import Communicator
 import tempfile
+import os
 
 comm = Communicator()
 
@@ -16,7 +17,6 @@ st.title("Subtitles Generator")
 st.header("Welcome to the Subtitles Generator!")
 st.subheader("This app allows you to generate translated subtitles for your videos.")
 
-st.write("Upload a video file:")
 uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mkv", "mov", "flv", "wmv", "webm", "ogg", "mp3", "wav", "flac", "aac", "m4a", "3gp", "3g2", "mj2", "wma", "wmv", "asf", "amv"])
 if uploaded_file is not None:
     st.write("Uploaded file:", uploaded_file)
@@ -24,9 +24,12 @@ if uploaded_file is not None:
     st.write("Type:", uploaded_file.type)
     st.write("Size:", uploaded_file.size)
 
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getbuffer())
-        video_path = tmp_file.name
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[1]) as tmp_file:
+            tmp_file.write(uploaded_file.getbuffer())
+            video_path = tmp_file.name
+    except:
+        st.error(f"An error occurred while saving the file: {uploaded_file.name}")
 
 lenguages = ["es", "en", "fr", "de", "it", "pt", "ru", "zh", "ja", "ko", "ar", "hi", "bn", "fa", "pl", "tr", "nl", "sv", "he", "da", "fi", "hu", "no", "ro", "uk", "vi", "ca", "el", "id", "ms", "th", "ur", "bg", "hr", "sr", "sk", "sl", "et", "lt", "lv", "mt", "eu", "gl", "cy", "sq", "be", "kk", "hy", "az", "ka", "mk", "mn", "nn", "ps", "tl", "pa", "so", "st", "ts", "sw", "uz", "af", "kab", "co", "haw", "yo", "is", "sw", "tl", "tg", "ta", "tt", "te", "ug", "uz", "ur", "yi", "zu"]
 target_language = st.selectbox("Target language", lenguages)
